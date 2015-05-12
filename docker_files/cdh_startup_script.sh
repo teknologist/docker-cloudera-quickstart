03922235cbf5cef9bfb4b6685ee2b578da24d98f
Eric Taïeb Walch <eric@teknologism.com>
2015-05-12 21:57:21 +0200

    Added missing start of new services

--- docker_files/cdh_startup_script.sh
+++ docker_files/cdh_startup_script.sh
@@ -26,6 +26,8 @@ nohup hiveserver2 &
 bash -c 'for x in `cd /etc/init.d ; ls impala-*` ; do sudo service $x start ; done'
 service hbase-master start
 service hbase-thrift start
+service cloudera-scm-server-db start
+service cloudera-scm-server start
 
 echo "Press Ctrl+P and Ctrl+Q to background this process."
 echo 'Use exec command to open a new bash instance for this instance (Eg. "docker exec -i -t CONTAINER_ID bash"). Container ID can be obtained using "docker ps" command.'
