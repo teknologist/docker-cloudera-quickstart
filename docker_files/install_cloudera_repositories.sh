5bedebefd4bcb3cc1644277d2c1d3ebdbd006667
Eric Taïeb Walch <eric@teknologism.com>
2015-05-12 22:28:30 +0200

    Added CM5 repos

--- docker_files/install_cloudera_repositories.sh
+++ docker_files/install_cloudera_repositories.sh
@@ -8,3 +8,8 @@ echo 'deb [arch=amd64] http://archive.cloudera.com/cdh5/ubuntu/trusty/amd64/cdh 
 echo 'deb-src http://archive.cloudera.com/cdh5/ubuntu/trusty/amd64/cdh trusty-cdh5 contrib' >> /etc/apt/sources.list.d/cloudera.list
 
 echo 'Cloudera repositories Installed.'
+# Packages for Cloudera Manager, Version 5, on Ubuntu 14.04 x86_64
+echo 'deb [arch=amd64] http://archive.cloudera.com/cm5/ubuntu/trusty/amd64/cm trusty-cm5 contrib' >> /etc/apt/sources.list.d/cloudera.list
+echo 'deb-src http://archive.cloudera.com/cm5/ubuntu/trusty/amd64/cm trusty-cm5 contrib' >> /etc/apt/sources.list.d/cloudera.list
+
+echo 'Cloudera Manager repositories Installed.'
